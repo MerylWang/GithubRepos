b089eecb384701aecbbdb18d15184e7a85915193
Mery <wangmeryl77@gmail.com>
2023-03-19 22:47:32 -0400

    remove hard-coded django key

--- gh_repos/gh_repos/settings.py
+++ gh_repos/gh_repos/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xu4ui@a3_jn!l^id$+w3(cb39!yk+xfld8v7qfo4de876s2!tk'
+# SECRET_KEY = 'django-insecure-xu4ui@a3_jn!l^id$+w3(cb39!yk+xfld8v7qfo4de876s2!tk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
